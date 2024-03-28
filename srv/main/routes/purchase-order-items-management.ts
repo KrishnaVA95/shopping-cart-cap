a232637bd8063a25c7c30a2f939061869b2b6dc3
krishna <krishnavarela.a@mail.com>
2024-03-28 08:23:12 -0300

    feat: insert price by quantity logic in cart

--- srv/main/routes/purchase-order-items-management.ts
+++ srv/main/routes/purchase-order-items-management.ts
@@ -1,16 +1,23 @@
 import '../configuration/module-alias';
 import { Request, Service } from '@sap/cds/apis/services';
 import { db } from '@/common/entities/db/models';
-
+import cds from '@sap/cds'
 // import { Products } from '../../../db/models';
 
 export default (service: Service) => {
-    service.after(['CREATE', 'READ', 'UPDATE'], 'PurchaseOrderItems', (results: db.models.PurchaseOrderItems[], request: Request)=>{
+    service.after(['CREATE', 'READ', 'UPDATE'], 'PurchaseOrderItems', async ( results: db.models.PurchaseOrderItems[], request: Request)=>{
 
+        
+        for(const current of results ){
+
+            const productQuery = SELECT.one.from(db.models.Entity.Products).where({
+                id:  current.product_id
+            })
+            const product = await cds.run(productQuery)
+
+            product ? current.price = current.quantity * product.price : current.price = null;
+        }
         results.forEach((current) => {
-            // const product =  Products.findOne({ where: { id: current.product_id } });
-            // product ? current.price = current.quantity * product.price : current.price = null;
-            current.price = 50
         })
     })
 }
